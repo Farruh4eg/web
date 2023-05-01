b2929d13e3df3632f7df8613ddf9d6de9ff69186
Farruh4eg <114315540+Farruh4eg@users.noreply.github.com>
2023-05-01 04:06:11 +0300

    garbage

--- Hacker News/script.js
+++ Hacker News/script.js
@@ -23,7 +23,8 @@ function prepareSecond(data) {
         author: data['by'],
         points: data['score'],
         id: data['id'],
-        time: data['time']
+        time: data['time'],
+        kids: data['kids']
     })
     count++;
     if (count === 100) {
@@ -36,11 +37,28 @@ function prepareSecond(data) {
 
 const container = document.getElementById('wrapper');
 
+if (document.getElementById('back')) {
+    document.getElementById('back').innerHTML = '';
+}
+if (document.getElementById('refresh')) {
+    document.getElementById('refresh').innerHTML = '';
+}
+
 function show(data) {
     container.innerHTML = '';
-    if(typeof data == 'string') {
+    if (typeof data == 'string') {
         data = JSON.parse(data);
     }
+    let nav = document.getElementById('nav');
+    nav.innerHTML = `<span class="hn">Hacker News</span>`;
+    let refresh = document.createElement('p');
+    refresh.innerText = 'Обновить';
+    refresh.id = 'refresh';
+    refresh.addEventListener('click', () => {
+        fetchFunc();
+        clearInterval(interval);
+    });
+    nav.append(refresh);
     for (let i in data) {
         let time = new Date(data[i]['time'] * 1000);
         time = time.toLocaleDateString('en-GB', { timezone: 'UTC', hour: 'numeric', minute: 'numeric', second: 'numeric' });
@@ -49,21 +67,86 @@ function show(data) {
         div.classList = 'news';
         div.id = data[i]['id'];
         div.innerHTML = `
-    <a href='${data[i]["link"]}'>${data[i]["title"]}</a>`;
+    <a class='link'>${data[i]["title"]}</a>`;
         secondDiv.classList = 'bot';
         secondDiv.innerHTML = `
-    ${data[i]["points"]} points by <a>${data[i]["author"]}</a> ${time}`;
+    ${data[i]["points"]} points by ${data[i]["author"]} ${time}`;
         div.append(secondDiv);
         container.append(div);
     }
-
+    [...document.getElementsByClassName('link')].forEach(element => {
+        element.addEventListener('click', (e) => {
+            showPage(e['target']['parentElement']['id']);
+        })
+    })
 }
 
 const interval = setInterval(() => {
     fetchFunc();
 }, 60000);
 
-document.getElementById('refresh').addEventListener('click', () => {
-    fetchFunc();
-    clearInterval(interval);
-});
+const commentAll = document.createElement('div');
+commentAll.id = 'commentAll';
+comment.classList = 'commentAll';
+
+function showPage(id) {
+    fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`)
+        .then(response => response.json())
+        .then(json => {
+            if (json['type'] === 'story') {
+                let data = json;
+                container.innerHTML = '';
+                if (document.getElementById('back')) {
+                    document.getElementById('back').innerHTML = '';
+                }
+                if (document.getElementById('refresh')) {
+                    document.getElementById('refresh').innerHTML = '';
+                }
+                let nav = document.getElementById('nav');
+                nav.innerHTML = `<span class="hn">Hacker News</span>`;
+                let back = document.createElement('p');
+                back.innerText = 'Назад';
+                back.id = 'back';
+                back.addEventListener('click', fetchFunc);
+                nav.append(back);
+                let refresh = document.createElement('p');
+                refresh.innerText = 'Обновить';
+                refresh.id = 'refresh';
+                refresh.addEventListener('click', () => {
+                    showPage(id);
+                    clearInterval(interval);
+                });
+                nav.append(refresh);
+                if (typeof data == 'string') {
+                    data = JSON.parse(data);
+                }
+                let time = new Date(data['time'] * 1000);
+                time = time.toLocaleDateString('en-GB', { timezone: 'UTC', hour: 'numeric', minute: 'numeric', second: 'numeric' });
+                const div = document.createElement('div');
+                const secondDiv = document.createElement('div');
+                div.classList = 'news';
+                div.id = data['id'];
+                div.innerHTML = `
+    <a href='${data['url']}' class='link'>${data["title"]}</a>`;
+                secondDiv.classList = 'bot';
+                secondDiv.innerHTML = `
+    ${data["score"]} points by ${data["by"]} ${time}`;
+                div.append(secondDiv);
+                container.append(div);
+                if (data['kids']) {
+                    const kids = data['kids'];
+                    kids.forEach(kid => showPage(kid));
+                }
+            } else if (json['type'] === 'comment') {
+                let received = json;
+                const comment = document.createElement('div');
+                comment.id = received['id'];
+                comment.classList = 'comment';
+                comment.innerHTML = `
+                            ${received['text']}`;
+                commentAll.append(comment);
+            }
+            container.append(commentAll);
+            setInterval(() => clearInterval(interval), 15);
+        });
+}
